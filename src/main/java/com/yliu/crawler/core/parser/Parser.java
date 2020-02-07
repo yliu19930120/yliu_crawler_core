d7edfe36e635a0e2c6d4731c23544b664b0c0e13
LiuYonghua <liuyonghua@ziasset.com>
2020-02-07 21:07:13 +0800

    修改依赖

--- src/main/java/com/yliu/crawler/core/parser/Parser.java
+++ src/main/java/com/yliu/crawler/core/parser/Parser.java
@@ -8,5 +8,12 @@ import com.yliu.crawler.core.exception.EllegalityUrlCatchtException;
 
 public interface Parser {
 	
+	/**
+	 * 
+	 * @param input 输入内容
+	 * @param source 抓取地址
+	 * @return
+	 * @throws EllegalityUrlCatchtException
+	 */
 	Page parse(String input,String source) throws EllegalityUrlCatchtException;
 }
